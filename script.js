517be319b26723a060cf5ebdfdf60b4a8ebaa5ae
Sameer Shaik <postspot@protonmail.com>
2025-12-31 15:52:39 +0530

    Lazy Loading images

--- script.js
+++ script.js
@@ -111,7 +111,6 @@ headerObserver.observe(header);
 
 const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer) {
-  console.log(entries);
   entries.forEach((entry) => {
     if (!entry.isIntersecting) return;
 
@@ -129,3 +128,31 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px', // to not let the user know that we are lazy loading
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
